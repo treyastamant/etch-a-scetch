fb4171ad1ab9d65099fefd5426079f0924d0d723
Treya St Amant <22247165+treyastamant@users.noreply.github.com>
2023-05-03 21:22:57 -0600

    Add hover effect to grid pixels

--- script.js
+++ script.js
@@ -7,11 +7,19 @@ function createGrid(h, w) {
   for (i = 0; i < x; i++) {
     const grid = document.createElement('div');
     grid.className = "grid";
+    grid.setAttribute('id', 'grid' + i);
     container.appendChild(grid);
+    
+    const pixel = document.querySelector('#grid' + i);
+    pixel.addEventListener('mouseover', () => {
+    pixel.setAttribute("style", "background-color:black;");
+    });
   }
 }
 
-const start = document.querySelector('#createGrid');
+const start = document.querySelector('#gridBtn');
 start.addEventListener('click', () => {
-  createGrid(16, 16);
-})
+  createGrid(16, 16)
+}, {once : true});
+
+
