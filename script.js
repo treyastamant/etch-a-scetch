bad4547111baa747ce0cf48cf9541a7ecac2d712
Treya St Amant <22247165+treyastamant@users.noreply.github.com>
2023-05-05 22:57:59 -0600

    Add pixel change functionality

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
 
 
-function createGrid(h, w) {
-  const x = h * w;
+function createGrid(x) {
   const container = document.querySelector('#container');
 
-  for (i = 0; i < x; i++) {
-    const grid = document.createElement('div');
-    grid.className = "grid";
-    grid.setAttribute('id', 'grid' + i);
-    container.appendChild(grid);
+  for (c = 1; c <= x; c++) {
+    const col = document.createElement('div');
+    col.className = 'col';
+    col.setAttribute('id', 'col' + c);
+    container.appendChild(col);
+  
+    for (r = 1; r <= x; r++) {
+      const row = document.createElement('div');
+      row.classList.add('row', 'row' + r, 'col' + c);
+      col.appendChild(row);
     
-    const pixel = document.querySelector('#grid' + i);
-    pixel.addEventListener('mouseover', () => {
-    pixel.setAttribute("style", "background-color:black;");
-    });
+      const pixel = document.querySelector('.row' + r, '.col' + c);
+      pixel.addEventListener('mouseover', () => {
+      pixel.setAttribute('style', 'background-color:black;');
+      });
+      console.log(c);
+      console.log(pixel);
+    }
+    
   }
 }
 
 const start = document.querySelector('#gridBtn');
 start.addEventListener('click', () => {
-  createGrid(32, 32)
+  createGrid(16)
 }, {once : true});
 
 
