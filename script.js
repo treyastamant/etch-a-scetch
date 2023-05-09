3ff602e99b451a1727060acfca33588e74da6f72
Treya St Amant <22247165+treyastamant@users.noreply.github.com>
2023-05-08 21:12:09 -0600

    Allow users to enter grid number

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+const gridBtn = document.querySelector('#gridBtn');
+gridBtn.addEventListener('click', () => {
+  let gridNum = prompt("Enter a number 1 - 100");
+  if (gridNum > 100) {prompt("Enter a number 1 - 100");}
+  createGrid(Number(gridNum));
+});
 
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+  }
+}
 
 function createGrid(x) {
   const container = document.querySelector('#container');
+  removeAllChildNodes(container);
 
   for (c = 1; c <= x; c++) {
     const col = document.createElement('div');
@@ -19,33 +31,31 @@ function createGrid(x) {
       pixel.addEventListener('mouseover', () => {
       pixel.setAttribute('style', 'background-color:black;');
       });
-      console.log(c);
-      console.log(pixel);
     }
     
   }
 }
 
-const toggle = document.querySelector('#gridBtn');
-toggle.addEventListener('click', () => {
-document.getElementById("myDropdown").classList.toggle("show");
-});
+// const toggle = document.querySelector('#gridBtn');
+// toggle.addEventListener('click', () => {
+// document.getElementById("myDropdown").classList.toggle("show");
+// });
 
-const grid32 = document.querySelector('#grid32');
-grid32.addEventListener('click', () => {
-  reset();
-  createGrid(32);
-  document.getElementById("myDropdown").classList.toggle("show");
-}, {once : true});
+// const grid32 = document.querySelector('#grid32');
+// grid32.addEventListener('click', () => {
+//   reset();
+//   createGrid(32);
+//   document.getElementById("myDropdown").classList.toggle("show");
+// }, {once : true});
 
-const grid64 = document.querySelector('#grid64');
-grid64.addEventListener('click', () => {
-  createGrid(64);
-  document.getElementById("myDropdown").classList.toggle("show");
-}, {once : true});
+// const grid64 = document.querySelector('#grid64');
+// grid64.addEventListener('click', () => {
+//   createGrid(64);
+//   document.getElementById("myDropdown").classList.toggle("show");
+// }, {once : true});
 
-const grid128 = document.querySelector('#grid128');
-grid128.addEventListener('click', () => {
-  createGrid(128);
-  document.getElementById("myDropdown").classList.toggle("show");
-}, {once : true});
+// const grid128 = document.querySelector('#grid128');
+// grid128.addEventListener('click', () => {
+//   createGrid(128);
+//   document.getElementById("myDropdown").classList.toggle("show");
+// }, {once : true});
